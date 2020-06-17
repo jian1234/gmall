45ba62ff29a6000cbbea8adb5cf7cbae3ada01ab
xiao <1435595618@qq.com>
2020-06-17 23:44:32 +0800

    redisson

---
+++ gmall-redisson-test/src/test/java/com/atguigu/gmall/redisson/GmallRedissonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.redisson;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallRedissonTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
