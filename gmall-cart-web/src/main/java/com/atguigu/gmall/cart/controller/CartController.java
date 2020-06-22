5f22186787cf3739bf64c45aaaf7d43fec52a71c
xiao <1435595618@qq.com>
2020-06-22 23:54:24 +0800

    购物车

---
+++ gmall-cart-web/src/main/java/com/atguigu/gmall/cart/controller/CartController.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.cart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CartController {
+
+    @RequestMapping("addToCart")
+    public String addToCart(){
+        return "redirect:/success.html";
+    }
+}
