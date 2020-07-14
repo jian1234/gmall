de92407aa149ce7324d85fb596932317cef3b408
xiao <1435595618@qq.com>
2020-07-14 23:43:58 +0800

    拦截器

--- gmall-web-util/src/main/java/com/atguigu/gmall/interceptors/AuthInterceptor.java
+++ gmall-web-util/src/main/java/com/atguigu/gmall/interceptors/AuthInterceptor.java
@@ -1,6 +1,10 @@
 package com.atguigu.gmall.interceptors;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.gmall.annotation.LoginRequired;
+import com.atguigu.gmall.util.CookieUtil;
+import com.atguigu.gmall.util.HttpclientUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
@@ -8,12 +12,15 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.lang.invoke.MethodHandle;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class AuthInterceptor extends HandlerInterceptorAdapter {
 
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,Object handle){
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,Object handle) throws IOException {
 
         //拦截代码
 
@@ -25,6 +32,63 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
             return true;
         }
         System.out.println("进入拦截方法");
+        String token  = "";
+        String oldToken = CookieUtil.getCookieValue(request,"oldToken",true);
+        if (StringUtils.isNotBlank(oldToken)){
+            token = oldToken;
+        }
+        String newToken = request.getParameter("token");
+        if (StringUtils.isNotBlank(newToken)){
+            token = newToken;
+        }
+        //调用认证中心
+        String success = "fail";
+        Map<String,String> successMap = new HashMap<>();
+        if (StringUtils.isNotBlank(token)){
+            String ip = request.getHeader("x-forwarded-for");// 通过nginx转发的客户端ip
+            if (StringUtils.isBlank(ip)){
+                ip = request.getRemoteAddr();
+                if(StringUtils.isBlank(ip)){
+                    ip = "127.0.0.1";
+                }
+            }
+            String successJson = HttpclientUtil.doGet("http://localhost:10005/verify?token=" + token+"&currentIp="+ip);
+            successMap = JSON.parseObject(successJson,Map.class);
+            success = successMap.get("status");
+        }
+
+
+        //获得请求是否必须登录
+        boolean loginSuccess = methodAnnotation.loginSuccess();
+
+        if (loginSuccess){
+            //必须登录
+            if (!success.equals("success")){
+                //进入验证中心  重新定向passport登录
+                StringBuffer requestURL = request.getRequestURL();
+                response.sendRedirect("http://localhost:10005/index?ReturnUrl="+requestURL);
+                return false;
+            }
+            // 需要将token携带的用户信息写入
+            request.setAttribute("memberId", successMap.get("memberId"));
+            request.setAttribute("nickname", successMap.get("nickname"));
+            if (StringUtils.isNotBlank(token)){
+                CookieUtil.setCookie(request,response,"oldToken",token,60*60*2,true);
+            }
+        }else {
+            // 没有登录也能用，但是必须验证
+            if (success.equals("success")) {
+                // 需要将token携带的用户信息写入
+                request.setAttribute("memberId", successMap.get("memberId"));
+                request.setAttribute("nickname", successMap.get("nickname"));
+
+                //验证通过，覆盖cookie中的token
+                if(StringUtils.isNotBlank(token)){
+                    CookieUtil.setCookie(request,response,"oldToken",token,60*60*2,true);
+                }
+
+            }
+        }
         return true;
     }
 }
